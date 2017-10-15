9d7203dc71c4d7e90ccfcf0a2e57cf4f19338435
James Gallagher <james@jamesgallagher.ie>
2017-10-15 01:45:07 +0100

    Fixing script issues that caused a non-zero exit. Partially related to the Dockerfile too

--- getCurrentReleaseOfResourceSpace.sh
+++ getCurrentReleaseOfResourceSpace.sh
@@ -7,14 +7,13 @@ if [ -f $CURRENT_RELEASE ]
 then
     rm $CURRENT_RELEASE
 fi
-TARGET_RELEASE_URL=1
 
-wget -q $TARGET_RELEASE_URL > /dev/null
+wget -q $TARGET_RELEASE_URL
 if [ $? = 0 ]
 then
     VERSION=`cat current_release`
     TARGET_SVN_URL="$RESOURCESPACE_SVN_BASE_URL$VERSION"
-    echo "The current version is $VERSIONxi; retrieving from svn"
+    echo "The current version is $VERSION; retrieving from svn"
 else
     exit 1
 fi
